6cdc893918563d66294912940860e759150dec8b
Malte Berneaud <malte.berneaud@gmail.com>
2016-03-21 10:40:56 +0100

    .gitignore and README, downloaded CMP data

---
+++ Data/downloading_manifestos_data.R
@@ -0,0 +1,21 @@
+############################
+# Eurosceptic Misfit Master Thesis
+# Author: Malte Berneaud-Kötz and Jonas Markgraf
+# Date created: 21.03.16
+# Last edited: 21.03.16
+# Contains the code used for downloading the Comparative Manifesto Project data
+
+# Loading the necessary libraries
+library(manifestoR)
+
+# setting API key and downloading entire data set
+# manifesto_apikey.txt can be obtained from the CMP website after signing up
+mp_setapikey("Data/manifesto_apikey.txt")
+data <- mp_maindataset()
+
+# Save the resulting data set
+write.csv(data, "Data/CMP.csv")
+
+# Creating a file containing the download date
+dateDownloaded <- date()
+write(dateDownloaded, "Data/CMP_download_date")
---
+++ README.md
@@ -0,0 +1,14 @@
+# Introduction to Collaborative Social Science Data Analysis
+This repo contains the results and the source files necessary for recreating the second pair assignment in the Collaborative Social Science Data Analysis.
+
+**Lecturer:** Christopher Gandrud
+
+## Assignment description
+[Assignment description in Christopher Gandrud's slides](https://github.com/HertieDataScience/SyllabusAndLectures/blob/master/LectureSlides/Lecture9/Lecture9.html)
+
+# Repo contents
+Work in progress. 
+
+## Collaborators
+Jonas Markgraf [GitHub](www.github.com/jmarkgraf)
+Malte Berneaud-Kötz [GitHub](www.github.com/mberneaud)
